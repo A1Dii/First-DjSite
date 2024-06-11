3b582c323db8c4b3f0b215638aa087b41f99423d
A1Dii <maks.zavyalov2016@yandex.ru>
2024-06-11 16:38:18 +0300

    add gitignore

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoSite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- one/settings.py
+++ one/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'one.urls'
+ROOT_URLCONF = 'DjangoSite.urls'
 
 TEMPLATES = [
     {
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'one.wsgi.application'
+WSGI_APPLICATION = 'DjangoSite.wsgi.application'
 
 
 
